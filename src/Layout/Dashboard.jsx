4f33463af19adc5b37f21b8f8969d839bdaead19
Md Alve <mdalve1091@gmail.com>
2023-06-08 19:17:28 +0600

    create useAdmin hook

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { FaBars, FaChalkboardTeacher, FaHome, FaUsers } from "react-icons/fa";
 import { NavLink, Outlet, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
-  const isAdmin = true;
+  // const isAdmin = true;
+  const [isAdmin] = useAdmin()
+  console.log(isAdmin);
+  const isInstructor = false;
 
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center bg-gradient-to-r from-[#0B0C10] to-[rgba(21, 21, 21, 0)]">
-      <label
+        <label
           htmlFor="my-drawer-2"
           className="btn btn-grad text-left drawer-button lg:hidden"
         >
-          <FaBars/>
+          <FaBars />
         </label>
         <Outlet></Outlet>
       </div>
@@ -21,28 +25,49 @@ const Dashboard = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full bg-[#0B0C10] text-white">
           {/* Sidebar content here */}
-          {isAdmin && (
-            <>
-              <li>
-                <NavLink
-                  to="/dashboard/adminHome"
-                  activeClassName="menu-item-active"
-                  className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
-                >
-                  <FaHome className="mr-2" />
-                  Admin Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/dashboard/manageUsers"
-                  activeClassName="menu-item-active"
-                  className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
-                >
-                  <FaUsers className="mr-2" />
-                  Manage Users
-                </NavLink>
-              </li>
+
+          <>
+            <li>
+              <NavLink
+                to={
+                  isAdmin
+                    ? `/dashboard/adminHome`
+                    : isInstructor
+                    ? "/dashboard/addClass"
+                    : "/dashboard/mySelectedClass"
+                }
+                activeClassName="menu-item-active"
+                className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
+              >
+                <FaHome className="mr-2" />
+                {isAdmin
+                  ? "Admin Home"
+                  : isInstructor
+                  ? "Add a Class"
+                  : "My Selected Classes"}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={
+                  isAdmin
+                    ? `/dashboard/manageUsers`
+                    : isInstructor
+                    ? "/dashboard/myClasses"
+                    : "/dashboard/myEnrolledClass"
+                }
+                activeClassName="menu-item-active"
+                className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
+              >
+                <FaUsers className="mr-2" />
+                {isAdmin
+                  ? "Manage Users"
+                  : isInstructor
+                  ? "My Classes"
+                  : "My Enrolled Classes"}
+              </NavLink>
+            </li>
+            {isAdmin && (
               <li>
                 <NavLink
                   to="/dashboard/manageClasses"
@@ -53,20 +78,19 @@ const Dashboard = () => {
                   Manage Classes
                 </NavLink>
               </li>
-              <hr className="text-white my-4" />
-              <li>
-                <NavLink
-                  to="/"
-                  activeClassName="menu-item-active"
-                  className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
-                >
-                  <FaHome className="mr-2" />
-                  Home
-                </NavLink>
-              </li>
-
-            </>
-          )}
+            )}
+            <hr className="text-white my-4" />
+            <li>
+              <NavLink
+                to="/"
+                activeClassName="menu-item-active"
+                className="flex items-center py-2 px-4 rounded transition-colors duration-200 hover:bg-[#66FCF1] hover:text-black"
+              >
+                <FaHome className="mr-2" />
+                Home
+              </NavLink>
+            </li>
+          </>
         </ul>
       </div>
     </div>
---
+++ src/hooks/useAdmin.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../Authentication/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+
+const useAdmin = () => {
+    const { user, loading } = useContext(AuthContext)
+    const [axiosSecure] = useAxiosSecure();
+    const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+        queryKey: ["isAdmin", user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const response = await axiosSecure.get(`/users/admin/${user?.email}`);
+            return response.data.admin;
+        },
+    });
+    return [isAdmin, isAdminLoading]
+};
+
+export default useAdmin;
