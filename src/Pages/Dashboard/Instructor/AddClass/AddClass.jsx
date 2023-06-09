6384eb2baf70e6280aff5e68c37c3857a60ab4d1
Md Alve <mdalve1091@gmail.com>
2023-06-09 11:18:37 +0600

    add class successfully

--- src/Pages/Dashboard/Instructor/AddClass/AddClass.jsx
+++ src/Pages/Dashboard/Instructor/AddClass/AddClass.jsx
@@ -1,34 +1,74 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../../../Authentication/AuthProvider";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AddClassForm = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
+  const [axiosSecure] = useAxiosSecure();
   const { user } = useContext(AuthContext);
+  const imageHostingToken = import.meta.env.VITE_Image_Upload_Token;
+  const imageHostingUrl = `https://api.imgbb.com/1/upload?key=${imageHostingToken}`;
 
   const onSubmit = (data) => {
     const {
       className,
       instructorName,
       instructorEmail,
-      classImage,
+      sportsCategory,
       availableSeats,
       price,
     } = data;
-    // get class information
-    const classInfo = {
-      className,
-      instructorName,
-      instructorEmail,
-      availableSeats: parseInt(availableSeats),
-      price: parseInt(price),
-      classImage
-    };
-    console.log(classInfo);
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    fetch(imageHostingUrl, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgResponse) => {
+        if (imgResponse.success) {
+          const imageURL = imgResponse.data.display_url;
+          // get class information
+          const newClass = {
+            className,
+            instructorName,
+            instructorEmail,
+            sportsCategory,
+            availableSeats: parseInt(availableSeats),
+            price: parseInt(price),
+            image: imageURL,
+          };
+          // post method to send data in database
+          fetch("http://localhost:5000/addClass", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              authorization: `bearer ${localStorage.getItem("access-token")}`,
+            },
+            body: JSON.stringify(newClass),
+          }).then((data) => {
+            console.log(data);
+            if (data) {
+              console.log('success');
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Class added successfully!",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              reset();
+            }
+          });
+        }
+      });
   };
 
   return (
@@ -135,19 +175,47 @@ const AddClassForm = () => {
             </div>
             <div>
               <label
-                htmlFor="classImage"
+                className="text-sm text-gray-800 font-medium"
+                htmlFor="sportsCategory"
+              >
+                Sports Category
+              </label>
+              <select
+                className={`block w-full px-4 py-2 mt-2 text-gray-700 border rounded-lg focus:outline-none focus:ring ${
+                  errors.price ? "border-red-500" : ""
+                }`}
+                id="sportsCategory"
+                {...register("sportsCategory", { required: true })}
+              >
+                <option value="">Select</option>
+                <option value="basketball">Basketball</option>
+                <option value="tennis">Tennis</option>
+                <option value="cricket">Cricket</option>
+                <option value="football">Football</option>
+                <option value="volleyball">Volleyball</option>
+                <option value="badminton">Badminton</option>
+                <option value="other">Other</option>
+              </select>
+              {errors.sportsCategory && (
+                <span className="error">Sports Category is required</span>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="image"
                 className="text-sm roboto font-medium text-gray-700"
               >
                 Class Image
               </label>
               <input
                 type="file"
-                id="classImage"
-                {...register("classImage", { required: true })}
-                className="border border-gray-300 rounded px-4 py-2 mt-2 focus:outline-none focus:ring"
+                id="image"
+                {...register("image", { required: true })}
+                className=" border border-gray-300 rounded px-4 py-2 mt-2 focus:outline-none focus:ring"
               />
 
-              {errors.classImage && (
+              {errors.image && (
                 <p className="text-red-500">Class Image is required.</p>
               )}
             </div>
