9b4fd92f28e96735b6923354100fe74968cc4dc4
Md Alve <mdalve1091@gmail.com>
2023-06-11 17:12:32 +0600

    popular classes section added

--- src/Home/PopularClasses/PopularClasses.jsx
+++ src/Home/PopularClasses/PopularClasses.jsx
@@ -1,24 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../Authentication/AuthProvider";
+import React, { useContext } from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { Circles } from "react-loader-spinner";
-import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Authentication/AuthProvider";
 
 const PopularClasses = () => {
-  const { user, loading } = useContext(AuthContext);
+    const {user,loading} = useContext(AuthContext)
   const [axiosSecure] = useAxiosSecure();
-  const navigate = useNavigate();
   const {
-    data: classes = [],
+    data: popularClasses = [],
     loading: isLoading,
-    refetch,
   } = useQuery({
-    queryKey: ["classes"],
+    queryKey: ["popularClasses"],
     enabled: !loading,
     queryFn: async () => {
-      const res = await axiosSecure.get("/classes");
+      const res = await axiosSecure.get(`/popularClasses`);
       return res.data;
     },
   });
@@ -35,13 +32,15 @@ const PopularClasses = () => {
       />
     );
 
+
+
   return (
     <div className="bg-gradient-to-r from-[#0B0C10] to-[rgba(21, 21, 21, 0)] px-7 py-12">
       <h2 className="text-3xl font-semibold text-white mb-6 text-left">
-        <span className="text-[#45A29E]">All</span> Classes
+        <span className="text-[#45A29E]">Popular</span> Classes
       </h2>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-        {classes.map((classItem) => (
+        {popularClasses.map((classItem) => (
           <div className="bg-gradient-to-r from-[#0B0C10] to-[#616161] rounded-lg shadow-lg p-6">
             <div className="mb-6">
               <img
@@ -52,6 +51,23 @@ const PopularClasses = () => {
             <h2 className="text-2xl font-semibold text-white">
               {classItem?.className}
             </h2>
+            <div className="mt-4">
+              <p className="text-gray-300">
+                <span className="font-semibold">Instructor:</span>{" "}
+                {classItem?.instructorName}
+              </p>
+              <p className="text-gray-300">
+                <span className="font-semibold">Available Seats:</span>{" "}
+                {classItem?.availableSeats}
+              </p>
+              <p className="text-gray-300">
+                <span className="font-semibold">Enrolled Students:</span>{" "}
+                {classItem?.enrolledStudents}
+              </p>
+              <p className="text-gray-300">
+                <span className="font-semibold">Price:</span>${classItem?.price}
+              </p>
+            </div>
           </div>
         ))}
       </div>
